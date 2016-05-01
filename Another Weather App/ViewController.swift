7086d0b4ccb065ddc29d75ada1a68a89fd8a65b6
MiniMac <MiniMac@mac-mini-minimac.local>
2016-05-01 10:36:19 +0200

    Initial Commit

---
+++ Another Weather App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Another Weather App
+//
+//  Created by MiniMac on 01.05.2016.
+//  Copyright © 2016 szmajdzinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
